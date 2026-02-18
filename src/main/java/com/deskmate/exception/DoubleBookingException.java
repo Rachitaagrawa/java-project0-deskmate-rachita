6acf5533ff6bfd1063657d2b815038ae91d734a4
Rachita Agrawal <rachita.agrawal77@gmail.com>
2026-02-18 12:08:07 +0530

    Adding Custom Exception

---
+++ src/main/java/com/deskmate/exception/DoubleBookingException.java
@@ -0,0 +1,10 @@
+package com.deskmate.exception;
+
+public class DoubleBookingException extends RuntimeException 
+{
+    public DoubleBookingException(String message) 
+    { 
+    	super(message); 
+    }
+}
+
