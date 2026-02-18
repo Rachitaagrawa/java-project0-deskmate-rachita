9c03f72a7dc8a0dea461ecc8a4f3ecdb58f7e135
Rachita Agrawal <rachita.agrawal77@gmail.com>
2026-02-18 13:16:46 +0530

    Update AppConfig class

--- src/main/java/com/deskmate/config/AppConfig.java
+++ src/main/java/com/deskmate/config/AppConfig.java
@@ -1,7 +1,17 @@
 package com.deskmate.config;
 
+import com.deskmate.controller.DeskController;
+import com.deskmate.dao.DeskDao;
+import com.deskmate.dao.implementation.JdbcDeskDao;
+import com.deskmate.services.DeskService;
+
 public class AppConfig 
 {
-    
+	public DeskController deskController() {
+        DeskDao deskDao = new JdbcDeskDao();
+        DeskService deskService = new DeskService(deskDao);
+        return new DeskController(deskService);
+    }
+
 }
 
