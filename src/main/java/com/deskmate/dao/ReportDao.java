d70cd333d55fec3a32ac2076870a480a18a378d6
Rachita Agrawal <rachita.agrawal77@gmail.com>
2026-02-19 00:27:26 +0530

    Add ReportDao class

---
+++ src/main/java/com/deskmate/dao/ReportDao.java
@@ -0,0 +1,12 @@
+package com.deskmate.dao;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.deskmate.model.report.DailyRevenueRow;
+import com.deskmate.model.report.DeskUtilizationRow;
+
+public interface ReportDao {
+    DailyRevenueRow dailyRevenue(LocalDate date);
+    List<DeskUtilizationRow> deskUtilization(LocalDate date);
+}
